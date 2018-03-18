b37584523b688c367e35c2c06a0df830064c2e75
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2018-03-18 20:33:30 +0100

    Add geojson-sanitize command

---
+++ bin/geojson-sanitize.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+'use strict'
+
+const {createGzip} = require('zlib')
+const yargs = require('yargs')
+const {pipe} = require('mississippi')
+const {parse, stringify} = require('../lib/streams')
+const {transform} = require('../lib/transform')
+
+const argv = yargs
+  .coerce('fromSrs', String)
+  .coerce('precision', Number.parseInt)
+  .argv
+
+const pipeline = [
+  parse(),
+  transform([
+    {op: 'unproj', from: argv.fromSrs},
+    {op: 'truncate', precision: argv.precision || 6}
+  ]),
+  stringify()
+]
+
+if (argv.compress) {
+  pipeline.push(createGzip())
+}
+
+pipe(process.stdin, ...pipeline, process.stdout, err => {
+  if (err) {
+    console.error('Error during processing!')
+    console.error(err)
+    process.exit(1)
+  }
+})
